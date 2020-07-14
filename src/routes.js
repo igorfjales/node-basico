c3aba1a66f33dd75b00a3bb2fc44512daaab8ccf
Igor Jales <igorjales_@hotmail.com>
2020-07-13 22:29:16 -0300

    Subida da minha primeira API criada em NodeJS.

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = express.Router();
+
+const ProductController = require("./controllers/ProductController");
+
+routes.get("/products", ProductController.read);
+
+routes.get("/products/:id", ProductController.showDetails);
+
+routes.post("/products", ProductController.create);
+
+routes.put("/products/:id", ProductController.update);
+
+routes.delete("/products/:id", ProductController.delete);
+
+module.exports = routes;
